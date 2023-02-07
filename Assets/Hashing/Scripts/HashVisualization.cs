fc43b40056142323d2e629310eb654bedde67dd0
Jorge Cobo <cobo-angel@hotmail.com>
2023-02-07 09:22:52 -0400

    Added Grid Transformation

--- Assets/Hashing/Scripts/HashVisualization.cs
+++ Assets/Hashing/Scripts/HashVisualization.cs
@@ -20,15 +20,27 @@ public class HashVisualization : MonoBehaviour
 
         public SmallXXHash hash;
 
+        public float3x4 domainTRS;
+
         public void Execute(int i)
         {
-            int v = (int)floor(invResolution * i + 0.00001f);
-            int u = i - resolution * v - resolution / 2;
-            v -= resolution / 2;
+            float vf = floor(invResolution * i + 0.00001f);
+            float uf = invResolution * (i - resolution * vf + 0.5f) - 0.5f;
+            vf = invResolution * (vf + 0.5f) - 0.5f;
+
+            float3 p = mul(domainTRS, float4(uf, 0f, vf, 1f));
+
+            int u = (int)floor(p.x);
+            int v = (int)floor(p.y);
+            int w = (int)floor(p.z);
             
-            hashes[i] = hash.Eat(u).Eat(v);
+            hashes[i] = hash.Eat(u).Eat(v).Eat(w);
         }
     }
+
+    [SerializeField] SpaceTRS domain = new SpaceTRS {
+        scale = 8f
+    };
 
     static int
         hashesId = Shader.PropertyToID("_Hashes"),
@@ -66,7 +78,8 @@ public class HashVisualization : MonoBehaviour
             hashes = hashes,
             resolution = resolution,
             invResolution = 1f / resolution,
-            hash = SmallXXHash.Seed(seed)
+            hash = SmallXXHash.Seed(seed),
+            domainTRS = domain.Matrix
         }.ScheduleParallel(hashes.Length, resolution, default).Complete();
 
         hashesBuffer.SetData(hashes);
