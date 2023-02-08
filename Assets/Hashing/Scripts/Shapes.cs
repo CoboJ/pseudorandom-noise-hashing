297250e9aed137c59c4c25382cc21a750af1f1b8
Jorge Cobo <cobo-angel@hotmail.com>
2023-02-08 09:18:20 -0400

    Shape responds properly to the transformations

--- Assets/Hashing/Scripts/Shapes.cs
+++ Assets/Hashing/Scripts/Shapes.cs
@@ -14,20 +14,23 @@ public static class Shapes
 
         public float resolution, invResolution;
 
+        public float3x4 positionsTRS;
+
         public void Execute(int i) {
             float2 uv;
             uv.y = floor(invResolution * i + 0.00001f);
             uv.x = invResolution * (i - resolution * uv.y + 0.5f) - 0.5f;
             uv.y = invResolution * (uv.y + 0.5f) - 0.5f;
 
-            positions[i] = float3(uv.x, 0f, uv.y);
+            positions[i] = mul(positionsTRS, float4(uv.x, 0f, uv.y, 1f));
         }
 
-        public static JobHandle ScheduleParallel(NativeArray<float3> positions, int resolution, JobHandle dependancy) {
+        public static JobHandle ScheduleParallel(NativeArray<float3> positions, int resolution, float4x4 trs, JobHandle dependancy) {
             return new Job {
                 positions = positions,
                 resolution = resolution,
-                invResolution = 1f / resolution
+                invResolution = 1f / resolution,
+                positionsTRS = float3x4(trs.c0.xyz, trs.c1.xyz, trs.c2.xyz, trs.c3.xyz)
             }.ScheduleParallel(positions.Length, resolution, dependancy);
         }
     }
